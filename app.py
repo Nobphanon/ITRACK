6f8cfc45ff612af90ca3e71c61cd103a6750e945
Nobphanon <6720301005@cdti.ac.th>
2025-12-27 02:49:39 +0700

    Update mail configuration to use SendGrid

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SESSION_PERMANENT'] = False
 
-# ================== 📧 Mail Config ==================
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# ================== 📧 Mail Config (SendGrid) ==================
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'kissmemore248@gmail.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = ('ITRACK Alert', app.config['MAIL_USERNAME'])
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = 'kissmemore248@gmail.com'
 
 mail.init_app(app)
 
